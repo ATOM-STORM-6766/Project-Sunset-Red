5223d94a802aac8e8399bc88a0302b70282103b5
vic123 <zhangzc_efz@163.com>
2024-05-12 10:06:08 +0800

    fix testauto

--- Sunset Red/src/main/java/frc/robot/auto/modes/TestAutoCommand.java
+++ Sunset Red/src/main/java/frc/robot/auto/modes/TestAutoCommand.java
@@ -1,22 +1,22 @@
 package frc.robot.auto.modes;
 
 import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.pathplanner.lib.path.PathPlannerPath;
+
+
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.DrivetrainSubsystem;
-import java.util.List;
 
 public class TestAutoCommand extends SequentialCommandGroup {
   public TestAutoCommand(DrivetrainSubsystem mDrivetrainSubsystem) {
-    List<PathPlannerPath> pathGroup = PathPlannerAuto.getPathGroupFromAutoFile("Example Auto");
+    PathPlannerPath startPath = PathPlannerPath.fromPathFile("Example Path");
     addCommands(
         mDrivetrainSubsystem.runZeroingCommand(),
         Commands.runOnce(
             () ->
-                mDrivetrainSubsystem.setPose(
-                    PathPlannerAuto.getStaringPoseFromAutoFile("Example Auto"))),
-        AutoBuilder.followPath(pathGroup.get(0)));
+                mDrivetrainSubsystem.setPose(startPath.getPreviewStartingHolonomicPose())),
+        AutoBuilder.followPath(startPath)
+    );
   }
 }
