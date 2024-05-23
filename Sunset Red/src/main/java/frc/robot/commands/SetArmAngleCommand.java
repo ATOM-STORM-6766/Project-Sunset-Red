9f06d1217f072cb2209ed32f1ddbb1cd54465718
GitHub Action <action@github.com>
2024-05-23 14:57:02 +0000

    Format Java code

--- Sunset Red/src/main/java/frc/robot/commands/SetArmAngleCommand.java
+++ Sunset Red/src/main/java/frc/robot/commands/SetArmAngleCommand.java
@@ -3,28 +3,28 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Arm;
 
-public class SetArmAngleCommand extends Command{
-    private final Arm sArm;
-    private final double targetAngle;
+public class SetArmAngleCommand extends Command {
+  private final Arm sArm;
+  private final double targetAngle;
 
-    public SetArmAngleCommand(Arm arm, double angle){
-        sArm = arm;
-        targetAngle = angle;
-        addRequirements(sArm);
-    }
+  public SetArmAngleCommand(Arm arm, double angle) {
+    sArm = arm;
+    targetAngle = angle;
+    addRequirements(sArm);
+  }
 
-    @Override
-    public void initialize(){
-        sArm.setAngle(targetAngle);
-    }
+  @Override
+  public void initialize() {
+    sArm.setAngle(targetAngle);
+  }
 
-    @Override
-    public void end(boolean interrupted){
-        sArm.stop();
-    }
+  @Override
+  public void end(boolean interrupted) {
+    sArm.stop();
+  }
 
-    @Override
-    public boolean isFinished(){
-        return sArm.getInPosition();
-    }
+  @Override
+  public boolean isFinished() {
+    return sArm.getInPosition();
+  }
 }
--- Sunset Red/src/main/java/frc/robot/commands/StopArmCommand.java
+++ Sunset Red/src/main/java/frc/robot/commands/StopArmCommand.java
@@ -4,15 +4,15 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.Arm;
 
 public class StopArmCommand extends InstantCommand {
-    private final Arm mArm;
+  private final Arm mArm;
 
-    public StopArmCommand(Arm arm) {
-        mArm = arm;
-        addRequirements(mArm);
-    }
+  public StopArmCommand(Arm arm) {
+    mArm = arm;
+    addRequirements(mArm);
+  }
 
-    @Override
-    public void initialize() {
-        mArm.stop();
-    }
+  @Override
+  public void initialize() {
+    mArm.stop();
+  }
 }
