a5c5920aab67a65e03c44a79792253190346bc07
GitHub Action <action@github.com>
2024-05-27 14:48:11 +0000

    Format Java code

--- Sunset Red/src/main/java/frc/robot/utils/ChassisSpeedKalmanFilterSimplified.java
+++ Sunset Red/src/main/java/frc/robot/utils/ChassisSpeedKalmanFilterSimplified.java
@@ -26,7 +26,7 @@ public class ChassisSpeedKalmanFilterSimplified {
 
   public ChassisSpeedKalmanFilterSimplified(double xyDev, double angleDev, double dt) {
     // state model deviation (Q) = 0
-    Vector<N3> stateStdDev = VecBuilder.fill(10.0,10.0,10.0);
+    Vector<N3> stateStdDev = VecBuilder.fill(10.0, 10.0, 10.0);
     // measurement deviation (R)
     Vector<N3> measurementStdDev = VecBuilder.fill(xyDev, xyDev, angleDev);
 
