364d2354fd2878c61b5b520da953226a827bfcd8
GONG ZERUI <gong0060@e.ntu.edu.sg>
2024-07-07 18:54:34 +0800

    WIP/ section based auto

--- Sunset Red/src/main/java/frc/robot/auto/modes/SectionBasedAutoCommand.java
+++ Sunset Red/src/main/java/frc/robot/auto/modes/SectionBasedAutoCommand.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathPlannerPath;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -14,7 +13,6 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
-import frc.robot.Constants.ArmConstants;
 import frc.robot.commands.ChaseNoteCommand;
 import frc.robot.commands.FeedCommand;
 import frc.robot.commands.IntakeCommand;
@@ -34,82 +32,77 @@ public class SectionBasedAutoCommand extends SequentialCommandGroup {
     private Arm sArm;
     private Transfer sTransfer;
     private DrivetrainSubsystem sDrivetrainSubsystem;
+
+    private boolean reachedPointB = false;
+
     public SectionBasedAutoCommand(
             Intake intake,
             Shooter shooter,
             Arm arm,
             Transfer transfer,
             DrivetrainSubsystem drivetrainSubsystem) {
-        sIntake = intake;
-        sShooter = shooter;
-        sArm = arm;
-        sTransfer = transfer;
-        sDrivetrainSubsystem = drivetrainSubsystem;
-    
+        this.sIntake = intake;
+        this.sShooter = shooter;
+        this.sArm = arm;
+        this.sTransfer = transfer;
+        this.sDrivetrainSubsystem = drivetrainSubsystem;
 
         addCommands(
-                // zeroing and shoot preload
-                new ParallelCommandGroup(
-                        new SequentialCommandGroup(
-                                sDrivetrainSubsystem.runZeroingCommand(),
-                                new InstantCommand(
-                                        () -> {
-                                            Optional<Alliance> a = DriverStation.getAlliance();
-                                            PathPlannerPath firstPath = PathPlannerPath.fromPathFile("HomeA to ObsA");
-                                            if (a.isPresent() && a.get() == Alliance.Red) {
-                                                firstPath = firstPath.flipPath();
-                                            }
-                                            sDrivetrainSubsystem.setPose(firstPath.getPreviewStartingHolonomicPose());
-                                        })),
-                        new SequentialCommandGroup(
-                                new ParallelCommandGroup(
-                                        new SetArmAngleCommand(sArm, ShootingParameters.BELOW_SPEAKER.angle_deg),
-                                        new SetShooterTargetCommand(
-                                                sShooter, ShootingParameters.BELOW_SPEAKER.speed_rps)),
+                new SequentialCommandGroup(
+                        sDrivetrainSubsystem.runZeroingCommand(),
+                        new InstantCommand(() -> {
+                            Optional<Alliance> a = DriverStation.getAlliance();
+                            PathPlannerPath firstPath = PathPlannerPath.fromPathFile("HomeA to ObsA");
+                            if (a.isPresent() && a.get() == Alliance.Red) {
+                                firstPath = firstPath.flipPath();
+                            }
+                            sDrivetrainSubsystem.setPose(firstPath.getPreviewStartingHolonomicPose());
+                        })),
+                new SequentialCommandGroup(
+                        // Initial setup and preload shot
+                        new ParallelCommandGroup(
+                                new SetArmAngleCommand(sArm, ShootingParameters.BELOW_SPEAKER.angle_deg),
+                                new SetShooterTargetCommand(sShooter, ShootingParameters.BELOW_SPEAKER.speed_rps)),
+                        new FeedCommand(sTransfer),
+                        new InstantCommand(() -> sShooter.stop()),
+                        new SetArmAngleCommand(sArm, Constants.ArmConstants.ARM_REST_ANGLE),
+
+                        // Move to first observe point and attempt to chase note
+                        buildPath("HomeA to ObsA").deadlineWith(new IntakeCommand(sIntake, sTransfer)),
+                        new ChaseNoteCommand(sDrivetrainSubsystem, GamePieceProcessor.getInstance(), sIntake, sTransfer),
+
+                        // If chase fails, move to second observe point
+                        new ConditionalCommand(
+                                new SequentialCommandGroup(
+                                        buildPath("ObsA to ObsB"),
+                                        new ChaseNoteCommand(sDrivetrainSubsystem, GamePieceProcessor.getInstance(),
+                                                sIntake, sTransfer),
+                                        new InstantCommand(() -> reachedPointB = true)),
+                                new InstantCommand(),
+                                () -> !sIntake.isOmronDetected()),
+
+                        // Return to shooting position
+                        new ConditionalCommand(
+                                buildPath("ReturnB to HomeB"),
+                                buildPath("ReturnA to HomeA"),
+                                () -> reachedPointB // Choose path based on which route it took
+                        )
+                                .alongWith(new SetShooterTargetCommand(sShooter,
+                                        ShootingParameters.BELOW_SPEAKER.speed_rps))
+                                .alongWith(new SetArmAngleCommand(sArm, ShootingParameters.BELOW_SPEAKER.angle_deg)),
+
+                        // Shoot if note was picked up
+                        new ConditionalCommand(
                                 new FeedCommand(sTransfer),
-                                new InstantCommand(
-                                        () -> {
-                                            sShooter.stop();
-                                        }),
-                                new SetArmAngleCommand(arm, Constants.ArmConstants.ARM_REST_ANGLE))),
+                                new InstantCommand(),
+                                () -> sTransfer.isOmronDetected()),
+                        new InstantCommand(() -> sShooter.stop())
 
-                // 1. Go from HomeA to ObsA
-                buildPath("HomeA to ObsA").deadlineWith(new IntakeCommand(sIntake, sTransfer)),
-
-                // 2. Start chase
-                new ChaseNoteCommand(sDrivetrainSubsystem, GamePieceProcessor.getInstance(), sIntake, sTransfer)
-                        .withTimeout(1),
-
-                // 3 & 4. If chase successful, go back and shoot. If not, go to ObsB
-                new ConditionalCommand(
-                        // If chase successful
-                        buildPathAndShoot("ReturnA to HomeA"),
-                        // If chase not successful
-                        new SequentialCommandGroup(
-                                buildPath("ObsA to ObsB"),
-                                // 5. Start chase again
-                                new ChaseNoteCommand(sDrivetrainSubsystem, GamePieceProcessor.getInstance(), sIntake,
-                                        sTransfer)
-                                        .withTimeout(1)),
-                        () -> sIntake.isOmronDetected()),
-
-                // 6. No matter the outcome of the second chase, go back and shoot
-                buildPathAndShoot("ReturnB to HomeB"));
+                ));
     }
 
     private Command buildPath(String pathName) {
         PathPlannerPath path = PathPlannerPath.fromPathFile(pathName);
         return AutoBuilder.followPath(path);
-    }
-
-    private Command buildPathAndShoot(String pathName) {
-        return new SequentialCommandGroup(
-                buildPath(pathName),
-                new ParallelCommandGroup(
-                        new SetArmAngleCommand(sArm, ShootingParameters.BELOW_SPEAKER.angle_deg),
-                        new SetShooterTargetCommand(sShooter, ShootingParameters.BELOW_SPEAKER.speed_rps)),
-                new FeedCommand(sTransfer),
-                new InstantCommand(() -> sShooter.stop()),
-                new SetArmAngleCommand(sArm, Constants.ArmConstants.ARM_REST_ANGLE));
     }
 }
