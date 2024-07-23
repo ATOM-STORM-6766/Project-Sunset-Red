dbd5a8a3b4885ce20d1ab631d7beb026dfab1e77
GONG ZERUI <89843462+Ry3nG@users.noreply.github.com>
2024-07-23 13:24:08 +0800

    fixed naming issue to align with convention

--- Sunset Red/src/main/java/frc/robot/lib6907/timeSwerveModulePositionSpeedGroup.java
+++ Sunset Red/src/main/java/frc/robot/lib6907/timeSwerveModulePositionSpeedGroup.java
@@ -3,12 +3,12 @@ package frc.robot.lib6907;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
-public class timeSwerveModulePositionSpeedGroup {
+public class TimeSwerveModulePositionSpeedGroup {
   public final SwerveModulePosition modulePosition;
   public final SwerveModuleState moduleState;
   public final double timestamp;
 
-  public timeSwerveModulePositionSpeedGroup(
+  public TimeSwerveModulePositionSpeedGroup(
       SwerveModulePosition modulePosition, SwerveModuleState moduleState, double timestamp) {
     this.modulePosition = modulePosition;
     this.moduleState = moduleState;
