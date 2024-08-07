2bd6188eb90f09df42c99818d25a7421fe4f59a2
rickxu2 <rickxu2@illinois.edu>
2024-08-07 12:47:56 +0800

    remove unused timer

--- Sunset Red/src/main/java/frc/robot/auto/AutoCommandFactory.java
+++ Sunset Red/src/main/java/frc/robot/auto/AutoCommandFactory.java
@@ -6,7 +6,6 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -125,7 +124,6 @@ public class AutoCommandFactory {
       GamePieceProcessor gamePieceProcessor,
       Command pathCommand,
       Rotation2d findNoteHeading) {
-    Timer timer = new Timer();
     return new SequentialCommandGroup(
         new ParallelDeadlineGroup(
             pathCommand,
