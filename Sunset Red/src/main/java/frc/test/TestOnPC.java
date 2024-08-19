31b661451363dc9fb0e0351aadef7a9ce3b7a9dd
rickxu2 <rickxu2@illinois.edu>
2024-08-19 21:47:08 +0800

    modified TestOnPC so it can run now

--- Sunset Red/src/main/java/frc/test/TestOnPC.java
+++ Sunset Red/src/main/java/frc/test/TestOnPC.java
@@ -2,20 +2,16 @@ package frc.test;
 
 import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.Constants.VisionShootConstants;
-import frc.robot.auto.AutoRoutineConfig.AutoShootPositions;
 
 public class TestOnPC {
     public static void main(String[] args) {
 
         // on blue side
-        Translation2d near_side = AutoShootPositions.NEAR_SIDE.shootPose.getTranslation().minus(VisionShootConstants.kBlueSpeaker);
-        Translation2d far_side = AutoShootPositions.FAR_SIDE.shootPose.getTranslation().minus(VisionShootConstants.kBlueSpeaker);
-        Translation2d under_stage = AutoShootPositions.UNDER_STAGE.shootPose.getTranslation().minus(VisionShootConstants.kBlueSpeaker);
-
-        Translation2d robotToGoal = near_side;
+        Translation2d position = new Translation2d(4.3, 6.3);
+        Translation2d robotToGoal = position.minus(VisionShootConstants.kBlueSpeaker);
 
         System.out.println("dist: "+ robotToGoal + "m");
-        System.out.println("heading" + robotToGoal.unaryMinus().getAngle().getDegrees());
+        System.out.println("heading" + robotToGoal.getAngle().getDegrees());
         System.out.println("Arm Angle:"+VisionShootConstants.kSpeakerAngleMap.get(robotToGoal.getNorm())+" deg");
         System.out.println("Shooter Speed:"+VisionShootConstants.kSpeakerRPSMap.get(robotToGoal.getNorm())+" rps");
 
